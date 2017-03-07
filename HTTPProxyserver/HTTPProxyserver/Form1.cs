8002a32f458a77ea66ea5ea5a3c5aa6a5d110761
Wijnand Honselaar <w.g.honselaar@gmail.com>
2017-03-07 17:46:08 +0100

    Minor changes

--- HTTPProxyserver/HTTPProxyserver/Form1.cs
+++ HTTPProxyserver/HTTPProxyserver/Form1.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Net;
-using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,7 +10,7 @@ namespace HTTPProxyserver
 {
     public partial class Form1 : Form
     {
-        private HttpListener _server = null;
+        private HttpListener _server;
 
         public Form1()
         {
@@ -41,12 +36,77 @@ namespace HTTPProxyserver
 
         private async void Listener()
         {
+            _server = new HttpListener();
+            var prefixes = GetPrefixes();
+            foreach (var p in prefixes)
+                _server.Prefixes.Add(p);
             _server.Start();
+
+            while (true)
+                try
+                {
+                    var ctx = await _server.GetContextAsync();
+                    Task.Run(() => ProcessRequest(ctx));
+                }
+                catch (HttpListenerException e)
+                {
+                    Console.WriteLine(e);
+                }
         }
 
-        private void ProcessRequest()
+        private IEnumerable<string> GetPrefixes()
         {
-
+            var prefixes = new List<string>
+            {
+                "http://*:" + txtPort.Text + "/"
+            };
+            return prefixes;
         }
+
+        private void ProcessRequest(HttpListenerContext ctx)
+        {
+            var methodName = ctx.Request.Url.Segments.Length > 1
+                ? ctx.Request.Url.Segments[1].Replace("/", "")
+                : "get";
+            var strParams = ctx.Request.Url
+                .Segments
+                .Skip(2)
+                .Select(s => s.Replace("/", ""))
+                .ToList();
+
+            Console.Write(ctx.Request.Headers);
+
+            HttpWebRequest request = WebRequest.Create(ctx.Request.Url) as HttpWebRequest;
+            if (request == null) return;
+            request.Accept = ctx.Request.Headers.Get("Accept");
+//            request.Connection = ctx.Request.Headers.Get("Connection");
+            request.ContentType = ctx.Request.ContentType;
+            request.ContentLength = ctx.Request.ContentLength64;
+            request.Host = ctx.Request.UserHostAddress;
+
+            HttpWebResponse response;
+            try
+            {
+                response = (HttpWebResponse) request.GetResponse();
+                PrintMessage("Host: " + response.Server);
+                PrintMessage("Status: " + response.StatusCode);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void PrintMessage(string message)
+        {
+            if (lstLog.InvokeRequired)
+            {
+                var d = new NewMessage(PrintMessage);
+                Invoke(d, message);
+            }
+            lstLog.Items.Add(message);
+        }
+
+        private delegate void NewMessage(string message);
     }
 }
