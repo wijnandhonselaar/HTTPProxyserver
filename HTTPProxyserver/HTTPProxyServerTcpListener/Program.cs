202a5adf5665a22872c1af51ec4f0ec752a12c5a
Wijnand Honselaar <w.g.honselaar@gmail.com>
2017-03-13 23:41:53 +0100

    Eerste zooi

--- HTTPProxyserver/HTTPProxyServerTcpListener/Program.cs
+++ HTTPProxyserver/HTTPProxyServerTcpListener/Program.cs
@@ -16,7 +16,7 @@ namespace HTTPProxyServerTcpListener
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ProxyUI());
         }
     }
 }
